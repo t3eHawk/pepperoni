06b7ff9005bd1b7ed434ae286c2698d9205df718
Тимур Фараджов <timurfaradzhov@gmail.com>
2020-05-31 12:44:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,34 @@
 """Setup."""
 
-import setuptools
 import pepperoni
+import setuptools
 
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
-
-install_requires = ['sqlalchemy>=1.3.1']
-
-
-name = 'pepperoni'
-author = pepperoni.__author__
-email = pepperoni.__email__
+name = pepperoni.__name__
 version = pepperoni.__version__
-description = 'Developer-friendly Python toolkit.'
+author = pepperoni.__author__
+author_email = pepperoni.__email__
+description = pepperoni.__doc__.splitlines()[0]
+long_description = open('README.md', 'r').read()
+long_description_content_type = 'text/markdown'
 license = pepperoni.__license__
+url = 'https://github.com/t3eHawk/pepperoni'
+install_requires = ['sqlalchemy']
+packages = setuptools.find_packages()
+classifiers = ['Programming Language :: Python :: 3',
+               'License :: OSI Approved :: MIT License',
+               'Operating System :: OS Independent']
 
 
-setuptools.setup(
-    name=name,
-    version=version,
-    author=author,
-    author_email=email,
-    description=description,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    license=license,
-    url='https://github.com/t3eHawk/pepperoni',
-    install_requires=install_requires,
-    packages=setuptools.find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-)
+setuptools.setup(name=name,
+                 version=version,
+                 author=author,
+                 author_email=author_email,
+                 description=description,
+                 long_description=long_description,
+                 long_description_content_type=long_description_content_type,
+                 license=license,
+                 url=url,
+                 install_requires=install_requires,
+                 packages=packages,
+                 classifiers=classifiers)
